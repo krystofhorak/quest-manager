46c498047e27a965213eb1ba5c1d686b9ad3cba6
krystofhorak <krystof.horak@proton.me>
2023-01-01 01:11:39 +0100

    BE: Connecting to PostgreSQL DB

--- server/src/routes/questRoutes.ts
+++ server/src/routes/questRoutes.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express';
 
+import { pool } from '../main';
+
 const questRouter = Router();
 
-const exampleQuestData: any[] = [
-  { quest_id: 0, name: 'Quest 1' },
-  { quest_id: 1, name: 'Quest 2' },
-  { quest_id: 2, name: 'Quest 3' },
-  { quest_id: 3, name: 'Quest 4' },
-  { quest_id: 4, name: 'Quest 5' },
-  { quest_id: 5, name: 'Quest 6' },
-  { quest_id: 6, name: 'Quest 7' },
-];
-
-questRouter.get('/', (_, res) => {
-  res.json(exampleQuestData);
+questRouter.get('/', async (_, res) => {
+  try {
+    const quests = await pool.query('SELECT * FROM Quests');
+    res.json(quests?.rows ?? []);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error!');
+  }
 });
 
 export default questRouter;
