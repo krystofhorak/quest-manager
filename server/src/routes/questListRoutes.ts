d7c6180fa1e3125d4636a00c9b832b27a2307b67
krystofhorak <krystof.horak@proton.me>
2023-01-01 18:06:32 +0100

    BE: Fetching questlist's quests along with the info of it

--- server/src/routes/questListRoutes.ts
+++ server/src/routes/questListRoutes.ts
@@ -17,8 +17,12 @@ questListRouter.get('/', async (_, res) => {
 questListRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const quest = await pool.query('SELECT * FROM Quests WHERE quest_id = $1', [id]);
-    res.json(quest?.rows?.[0] ?? null);
+    const questList = await pool.query('SELECT * FROM QuestLists WHERE questlist_id = $1', [id]);
+    const quests = await pool.query('SELECT QuestLists.questlist_id, Quests.* FROM QuestLists INNER JOIN Quests ON QuestLists.questlist_id = Quests.questlist WHERE questlist_id = $1', [id]);
+    res.json({
+      ...questList?.rows?.[0],
+      quests: quests?.rows ?? [],
+    } ?? null);
   } catch (error) {
     console.log(error);
     res.status(500).send('Error!');
